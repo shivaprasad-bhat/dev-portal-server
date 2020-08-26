71fa130c843595427f090d1cbc0d8828a295922b
Shivaprasad Bhat <shivaprasad.bhat@learner.manipal.edu>
2020-08-26 19:41:05 +0530

    adding module 1 - Landing Page, Register and Login

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <section className="container">
+                        <Switch>
+                            <Route
+                                exact
+                                path="/register"
+                                component={Register}
+                            />
+                            <Route exact path="/login" component={Login} />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
+};
+
+export default App;
